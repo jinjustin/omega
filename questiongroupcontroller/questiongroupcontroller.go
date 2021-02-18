5d4e029a7c6f2adfd62fd5a3dd4905a4c64abc83
jinjustin <jindefery@gmail.com>
2021-02-19 03:14:39 +0700

    Update questiongroupcontroller.go

--- questiongroupcontroller/questiongroupcontroller.go
+++ questiongroupcontroller/questiongroupcontroller.go
@@ -117,7 +117,7 @@ func allGroupTestList(courseID string, testID string) []byte{
 	}
 	defer db.Close()
 
-	sqlStatement := `SELECT uuid FROM questiongroup WHERE course=$1 and testid=$2`
+	sqlStatement := `SELECT uuid FROM questiongroup WHERE courseid=$1 and testid=$2`
 	rows, err := db.Query(sqlStatement, testID)
 	if err != nil {
 		panic(err)
