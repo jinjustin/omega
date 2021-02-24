e4206916eb38c709ccc1feb7889decd06a8ef69c
jinjustin <jindefery@gmail.com>
2021-02-25 04:11:16 +0700

    test bank API

--- questiongroupcontroller/questiongroupcontroller.go
+++ questiongroupcontroller/questiongroupcontroller.go
@@ -96,6 +96,35 @@ func groupTestListUpdate(name string, questiongroupID string, questiongroupName 
 	}
 }
 
+func testbankUpdate(name string, questiongroupID string, questiongroupName string, numQuestion string, score string, courseID string, uuid string) {
+	
+	var g questiongroup.QuestionGroup
+
+	g = questiongroup.QuestionGroup{
+		Name: name,
+		ID: questiongroupID,
+		GroupName: questiongroupName,
+		NumQuestion: numQuestion,
+		Score: score,
+		CourseID: courseID,
+		TestID: "",
+		UUID: uuid,
+	}
+
+	db, err := sql.Open("postgres", database.PsqlInfo())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `UPDATE questiongroup SET name=$1, groupname=$2, numquestion=$3, score=$4, uuid=$5 WHERE id=$6`
+
+	_, err = db.Exec(sqlStatement, g.Name, g.GroupName, g.NumQuestion, g.Score, g.UUID, g.ID)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func getGroupInTest(courseID string, testID string) []byte{
 
 	type GroupItem struct {
@@ -196,6 +225,106 @@ func getGroupInTest(courseID string, testID string) []byte{
 	return b
 }
 
+func getGroupInTestbank(courseID string) []byte{
+
+	type GroupItem struct {
+		ID string
+		GroupName string
+		NumQuestion string
+		Score string
+	}
+
+	type GroupInTest struct {
+		Name string
+		Items []GroupItem
+	}
+
+	groupTestMap := make(map[string]GroupInTest)
+
+	var UUIDs []string
+	var uuid string
+
+	var GroupItems []GroupItem
+
+	var g GroupInTest
+	var i GroupItem
+
+	db, err := sql.Open("postgres", database.PsqlInfo())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `SELECT uuid FROM questiongroup WHERE courseid=$1 and testid=''`
+	rows, err := db.Query(sqlStatement, courseID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&uuid)
+		if err != nil {
+			panic(err)
+		}
+		UUIDs = append(UUIDs, uuid)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, uuid := range UUIDs {
+
+		sqlStatement := `SELECT name FROM questiongroup WHERE uuid=$1 and testid=''`
+		rows, err := db.Query(sqlStatement, uuid)
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			err = rows.Scan(&g.Name)
+			if err != nil {
+				panic(err)
+			}
+		}
+		err = rows.Err()
+		if err != nil {
+			panic(err)
+		}
+
+		sqlStatement = `SELECT id, groupname, numquestion, score FROM questiongroup WHERE uuid=$1 and testid=''`
+		rows, err = db.Query(sqlStatement, uuid)
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			err = rows.Scan(&i.ID, &i.GroupName, &i.NumQuestion, &i.Score)
+			if err != nil {
+				panic(err)
+			}
+			GroupItems = append(GroupItems, i)
+		}
+		err = rows.Err()
+		if err != nil {
+			panic(err)
+		}
+
+		g.Items = GroupItems
+		groupTestMap[uuid] = g
+	}
+
+	b,err := json.Marshal(groupTestMap)
+	if err != nil{
+		panic(err)
+	}
+
+	return b
+}
+
 func checkQuestionGroupExist(questionGroupID string) bool {
 
 	var uuid string
@@ -286,10 +415,9 @@ func deleteQuestionGroupFromTest(questionInTest []string, testID string, courseI
 	
 }
 
-func deleteQuestionGroupFromTestbank(id string) []byte{
-	var q questiongroup.QuestionGroup
+func deleteQuestionGroupFromTestbank(questionInTest []string, courseID string){
 
-	q.ID = id
+	var questionID string
 
 	db, err := sql.Open("postgres", database.PsqlInfo())
 	if err != nil {
@@ -297,31 +425,39 @@ func deleteQuestionGroupFromTestbank(id string) []byte{
 	}
 	defer db.Close()
 
-	sqlStatement := `SELECT name, groupname, numquestion, score, courseid, testid, uuid  FROM questiongroup WHERE id=$1;`
-	rows, err := db.Query(sqlStatement, id)
+	sqlStatement := `SELECT id FROM questiongroup WHERE courseid=$1;`
+	rows, err := db.Query(sqlStatement, courseID)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		err = rows.Scan(&q.Name, &q.GroupName, &q.NumQuestion, &q.Score, &q.CourseID, &q.TestID, &q.UUID)
+		err = rows.Scan(&questionID)
 		if err != nil {
 			panic(err)
+		}
+
+		check := true
+
+		for _, id := range questionInTest{
+			if questionID == id{
+				check = false
+			}
+		}
+
+		if check {
+			sqlStatement := `DELETE from questiongroup WHERE id=$1;`
+			_, err = db.Exec(sqlStatement, questionID)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 	err = rows.Err()
 	if err != nil {
 		panic(err)
 	}
-
-	sqlStatement = `DELETE from questiongroup WHERE id=$1;`
-	_, err = db.Exec(sqlStatement, id)
-	if err != nil {
-		panic(err)
-	}
-
-	return q.GetQuestionGroupDetail()
 }
 
 func allgrouptestlist(courseid string) []GrouptestList{
@@ -413,18 +549,61 @@ var GetGroupInTest = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reques
 	w.Write(getGroupInTest(courseID,testID))
 })
 
-//DeleteGroupInTestbank is a API that use to delete 
-var DeleteGroupInTestbank = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-	ID := r.Header.Get("id")
-
-	w.Write(deleteQuestionGroupFromTestbank(ID))
-})
-
 //AllGroupTestList is a API that use to get all grouptestlist in that course 
 var AllGroupTestList = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	courseID := r.Header.Get("CourseID")
 
 	json.NewEncoder(w).Encode(allgrouptestlist(courseID))
+})
+
+//TestbankUpdate is a API that use to get add or update group test list.
+var TestbankUpdate = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	type Item struct {
+		ID string
+		GroupName string
+		NumQuestion string
+		Score string
+	}
+
+	type Input struct {
+		Name string
+		Items []Item
+	}
+
+	var objmap map[string]Input
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	//var input Input
+	json.Unmarshal(reqBody, &objmap)
+	uuids := make([]string, 0, len(objmap))
+	for i := range objmap {
+        uuids = append(uuids, i)
+    }
+
+	var input Input
+
+	var questionInTest []string
+
+	courseID := r.Header.Get("CourseID")
+
+	for _, uuid := range uuids {
+		input = objmap[uuid]
+		for _, item := range input.Items{
+			testbankUpdate(input.Name, item.ID, item.GroupName, item.NumQuestion, item.Score, courseID, uuid)
+			questionInTest = append(questionInTest, item.ID)
+		}
+	}
+
+	deleteQuestionGroupFromTestbank(questionInTest, courseID)
+	w.Write([]byte("success"))
+})
+
+//GetGroupInTestbank is a API that use to get all questiongroup in the testbsnk.
+var GetGroupInTestbank = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	courseID := r.Header.Get("CourseID")
+
+	w.Write(getGroupInTestbank(courseID))
 })
